34d8c009407af6c267b3405286f638467254045a
Davood Rahmati <rahmatidavood23@gmail.com>
2023-08-14 16:01:09 +0330

    - test1

--- users/models.py
+++ users/models.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 class User(AbstractBaseUser):
-    full_name = models.CharField()
+    full_name = models.CharField(max_length=50)
     mobile = models.CharField(max_length=11, unique=True)
     date_of_birth = models.DateTimeField()
     national_code = models.IntegerField()
     email = models.EmailField(max_length=255, unique=True)
-    sheba_bank = models.CharField()
-    address_1 = models.CharField()
-    address_2 = models.CharField()
-    address_3 = models.CharField()
-    message = models.CharField()
+    sheba_bank = models.CharField(max_length=50)
+    address_1 = models.CharField(max_length=255)
+    address_2 = models.CharField(max_length=255)
+    address_3 = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
     USERNAME_FIELD = 'mobile'
     REQUIRED_FIELDS = ['email']
     is_admin = models.BooleanField(default=False)
